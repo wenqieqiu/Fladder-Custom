6e9066ddfae9afff726915b2477551ac15379c40
v3DJG6GL <72495210+v3DJG6GL@users.noreply.github.com>
2026-04-28 19:45:00 +0200

    fix(web): container ENV vars unreliably applied due to startup race (#972)

--- lib/bootstrap/platform/web_app_wrapper.dart
+++ lib/bootstrap/platform/web_app_wrapper.dart
@@ -1,12 +1,7 @@
-import 'dart:convert';
-
-import 'package:flutter/services.dart';
-
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:universal_html/html.dart' as html;
 
 import 'package:fladder/bootstrap/platform/base_app_wrapper.dart';
-import 'package:fladder/util/fladder_config.dart';
 
 class WebAppWrapper extends BaseAppWrapper {
   const WebAppWrapper({super.key, required super.builder});
@@ -22,12 +17,5 @@ class _WebAppWrapperState extends BaseAppWrapperState<WebAppWrapper> {
   @override
   Future<void> platformInit() async {
     html.document.onContextMenu.listen((event) => event.preventDefault());
-    final result = await _loadConfig();
-    FladderConfig.fromJson(result);
-  }
-
-  Future<Map<String, dynamic>> _loadConfig() async {
-    final configString = await rootBundle.loadString('config/config.json');
-    return jsonDecode(configString);
   }
 }
